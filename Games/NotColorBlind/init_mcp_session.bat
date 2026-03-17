203c2c6951698b553f7089078bed9e8b28c1f65b
yijianyouxie <haibinxiaochenga@163.com>
2026-03-17 17:30:58 +0800

    AI Code和编辑器设置。

--- Games/NotColorBlind/init_mcp_session.bat
+++
@@ -1,5 +0,0 @@
-off
-powershell
--Command
-Invoke-WebRequest -Uri 'http://localhost:8080/' -Method 'POST' -Headers @{'^Content-Type^'='application/json'} -Body '{\
-jsonrpc\:\2.0\,\id\:1,\method\:\initialize\,\params\:{\protocolVersion\:\2024-11-05\,\capabilities\:{},\clientInfo\:{\name\:\PowerShell\,\version\:\1.0.0\}}}' -UseBasicParsing
